6e6140fd936aebfb9f1da0fe05ca0275028718e7
Kill Toxic <913087364@qq.com>
2020-05-22 14:31:15 +0800

    backgroundDifference

--- backgroundDifference/backgroundDifference.cpp
+++ backgroundDifference/backgroundDifference.cpp
@@ -1,20 +1,52 @@
-﻿// backgroundDifference.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	VideoCapture cap(0);
+
+	//如果摄像头打开失败
+	if (!cap.isOpened())
+	{
+		std::cout << "Fail to open camera" << std::endl;
+		return -1;
+	}
+
+	int cnt = 0;
+
+	cv::Mat frame;
+	cv::Mat bgMat;
+	cv::Mat subMat;
+	cv::Mat bny_subMat;
+
+
+	while (1)
+	{
+		cap >> frame;
+		cvtColor(frame, frame, COLOR_BGR2GRAY);
+
+		if (cnt == 0)
+		{
+			//第一帧，获得背景图像
+			frame.copyTo(bgMat);
+		}
+		else
+		{
+			//第二帧开始背景差分
+			absdiff(frame, bgMat, subMat);
+			//差分结果二值化
+			threshold(subMat, bny_subMat, 20, 255, CV_THRESH_BINARY);
+
+			cv::imshow("b_subMat", bny_subMat);
+			cv::imshow("frame", frame);
+			waitKey(30);
+		}
+		cnt++;
+	}
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
